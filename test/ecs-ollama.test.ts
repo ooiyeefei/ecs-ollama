69dc66675832a8f2a9a064256caff2e9d2875de4
Ooi Yee Fei <yeefeiooi@gmail.com>
2024-04-30 23:44:41 +0800

    Initial commit

---
+++ test/ecs-ollama.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as EcsOllama from '../lib/ecs-ollama-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/ecs-ollama-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new EcsOllama.EcsOllamaStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
